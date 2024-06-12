b64ac5a1cc16b4a9098312ecb9f76669cf89f181
Dzung26Vu <122276504+Dzung26Vu@users.noreply.github.com>
2024-06-13 04:09:38 +0700

    test table user

--- login-app/src/Home.jsx
+++ login-app/src/Home.jsx
@@ -1,8 +1,45 @@
+import  { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Gọi API để lấy dữ liệu người dùng từ máy chủ
+    fetch('https://test-dbonline-y5jx.vercel.app/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
   return (
     <div>
-      <h2>Welcome to Home Page</h2>
+      <h2>User Information</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.createdAt}</td>
+              <td>{user.updatedAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
