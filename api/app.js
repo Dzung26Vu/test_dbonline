304d2a3535888c6858f499b374b5f8710716da3b
Dzung26Vu <122276504+Dzung26Vu@users.noreply.github.com>
2024-06-13 03:47:40 +0700

    aa

--- api/app.js
+++ api/app.js
@@ -8,94 +8,89 @@ const app = express();
 const port = 3000;
 const SECRET_KEY = 'your_secret_key';
 
-// Khởi tạo kết nối tới cơ sở dữ liệu từ biến môi trường DATABASE_URL
 const DATABASE_URL = "postgresql://dzungvu:3Pm6J_QgrX6tGA-EpNstMQ@dbdemo-7014.6xw.aws-ap-southeast-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full";
-
 
 const sequelize = new Sequelize(DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Note: This might expose you to man-in-the-middle attacks
-      }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Note: This might expose you to man-in-the-middle attacks
+        }
     }
 });
 
-// Define the User model according to the new table structure
 const User = sequelize.define('users', {
-  id: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    defaultValue: Sequelize.literal('unique_rowid()')
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  createdAt: {
-    field: 'created_at',
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  },
-  updatedAt: {
-    field: 'updated_at',
-    type: DataTypes.DATE,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  }
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('unique_rowid()')
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        field: 'created_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        field: 'updated_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    }
 }, {
-  // Sequelize options
-  tableName: 'users',
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
+    tableName: 'users',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// Register a new user
 app.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const newUser = await User.create({ username, email, password });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Error registering user' });
-  }
+    const { username, email, password } = req.body;
+    try {
+        const newUser = await User.create({ username, email, password });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Error registering user' });
+    }
 });
 
-// Login
 app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(401).json({ error: 'User not found' });
-    }
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
 
-    if (user.password !== password) {
-      return res.status(401).json({ error: 'Incorrect password' });
-    }
+        if (user.password !== password) {
+            return res.status(401).json({ error: 'Incorrect password' });
+        }
 
-    const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
-    res.json({ token });
-  } catch (error) {
-    console.error(error); // Log the error for debugging purposes
-    res.status(500).json({ error: 'Error processing login request' });
-  }
+        const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error processing login request' });
+    }
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
