e7bd8183dc36d845a7366f63068fb6a3416b9d4f
Dzung26Vu <122276504+Dzung26Vu@users.noreply.github.com>
2024-06-13 20:40:44 +0700

    darkmode tailwindcss

--- login-app/src/App.jsx
+++ login-app/src/App.jsx
@@ -1,17 +1,52 @@
-
+import  { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+function App() {
+	const [theme, setTheme] = useState(null);
+
+	useEffect(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	const handleThemeSwitch = () => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+	return (
+		<>
+			<button
+				type="button"
+				onClick={handleThemeSwitch}
+				className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
+			>
+				{theme === 'dark' ? '🌙' : '🌞'}
+			</button>
+			<div className="bg-white dark:bg-slate-900 min-h-screen font-inter ">
+        <div className="max-w-5xl w-11/12 mx-auto">
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
--- login-app/src/Home.jsx
+++ login-app/src/Home.jsx
@@ -2,44 +2,100 @@ import  { useState, useEffect } from 'react';
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  // Fetch danh sách người dùng từ API
   useEffect(() => {
-    // Gọi API để lấy dữ liệu người dùng từ máy chủ
     fetch('https://test-dbonline-y5jx.vercel.app/users')
       .then(response => response.json())
-      .then(data => {
-        setUsers(data);
-      })
-      .catch(error => {
-        console.error('Error fetching users:', error);
-      });
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
   }, []);
 
+  // Xử lý thay đổi ô tìm kiếm
+  const handleSearchChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Lọc danh sách người dùng dựa trên từ khóa tìm kiếm
+  const filteredUsers = users.filter(user =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div>
-      <h2>User Information</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Created At</th>
-            <th>Updated At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.createdAt}</td>
-              <td>{user.updatedAt}</td>
+    <div className="container mx-auto p-4   ">
+      <h2 className="text-2xl font-bold mb-4">User Information</h2>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="pb-4 bg-white dark:bg-gray-900">
+          <label htmlFor="table-search" className="sr-only">Search</label>
+          <div className="relative mt-1">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            </div>
+            <input
+              type="text"
+              id="table-search"
+              className="block w-80 px-10 py-2 text-sm text-gray-900 placeholder-gray-400 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for items"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </div>
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Username
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Created At
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Updated At
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredUsers.map(user => (
+              <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input id={`checkbox-table-search-${user.id}`} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label htmlFor={`checkbox-table-search-${user.id}`} className="sr-only">checkbox</label>
+                  </div>
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {user.id}
+                </td>
+                <td className="px-6 py-4">
+                  {user.username}
+                </td>
+                <td className="px-6 py-4">
+                  {user.email}
+                </td>
+                <td className="px-6 py-4">
+                  {user.createdAt}
+                </td>
+                <td className="px-6 py-4">
+                  {user.updatedAt}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
